f224ecdd01c7d77e7483ceba0524ec79e6a8efae
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-11-19 18:44:32 -0500

    Update user_model.class.php

--- models/user_model.class.php
+++ models/user_model.class.php
@@ -5,7 +5,7 @@
  * File: user_model.class.php
  * Description: This class handles user data interactions with the database
  */
-lass UserModel {
+class UserModel {
     private Database $db; // Database object
     private mysqli $dbConnection; // Database connection object
 
